2f023b4919f5e5718f01770a0bd135b0ce0c6949
AKD <akdusa20@gmail.com>
2024-05-02 22:15:13 -0400

    commit'

--- JS/index.js
+++ JS/index.js
@@ -1,57 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
     const animationsPlayed = sessionStorage.getItem('animationsPlayed');
+    const slideshow = document.querySelector(".container");
+    slideshow.classList.add("start-slideshow");
 
     function runAnimations() {
-        // Intersection Observer for 'h1' elements
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('title-fade');
-                }
-            });
+        const texts = document.querySelectorAll(".text");
+        let delay = 450;
+
+        texts.forEach((text, index) => {
+            setTimeout(() => {
+                text.classList.add('show');
+                text.style.transitionDelay = `${index * 0.5}s`; // Adjust the delay for each text element
+            }, delay);
+            delay += 450; // 1000 ms
+            sessionStorage.setItem('animationsPlayed', true);
         });
-
-        const hiddenElements = document.querySelectorAll('h1');
-        hiddenElements.forEach((el) => observer.observe(el));
-
-        // Intersection Observer for 'p' elements
-        const observer1 = new IntersectionObserver((entries1) => {
-            entries1.forEach((entry1) => {
-                if (entry1.isIntersecting) {
-                    entry1.target.classList.add('p-fade');
-                }
-            });
-        });
-
-        const hiddenElements1 = document.querySelectorAll('p');
-        hiddenElements1.forEach((el) => observer1.observe(el));
-
-        // Intersection Observer for '.btn' elements
-        const observer2 = new IntersectionObserver((entries2) => {
-            entries2.forEach((entry2) => {
-                if (entry2.isIntersecting) {
-                    entry2.target.classList.add('btn-fade');
-                }
-            });
-        });
-
-        const hiddenElements2 = document.querySelectorAll('.btn');
-        hiddenElements2.forEach((el) => observer2.observe(el));
-    
-        sessionStorage.setItem('animationsPlayed', true);
     }
 
-    if (!animationsPlayed || window.performance && performance.navigation.type === 1) {
+    if (!animationsPlayed || (window.performance && performance.navigation.type === 1)) {
         runAnimations();
+    } else {
+        const texts = document.querySelectorAll(".text");
+
+        texts.forEach((text, index) => {
+            text.classList.add('show');
+        });
     }
 });
 
 /* Loading */
-var preloader = document.getElementById("preloader")
+var preloader = document.getElementById("preloader");
 
 window.addEventListener("load", removePreloader);
 
 function removePreloader() {
-  preloader.classList.add("removePreloader");
+    preloader.classList.add("removePreloader");
 }
-
